36e4c59d359aa6c887d4ff3eec4e0f2c9215eecf
stef0296 <steff.alvares@gmail.com>
2023-02-26 11:54:47 +0100

    Updated list of injectable providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CsvService } from './csv/csv.service';
+import { FtpService } from './ftp/ftp.service';
+import { TranslateService } from './translate/translate.service';
+import { ShopifyService } from './shopify/shopify.service';
+import { DbService } from './db/db.service';
+import { SchedulerService } from './scheduler/scheduler.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    CsvService,
+    FtpService,
+    TranslateService,
+    ShopifyService,
+    DbService,
+    SchedulerService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
