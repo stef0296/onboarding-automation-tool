a7f5253bddefcf43b2a32eb889ceb1fd5119c9be
stef0296 <steff.alvares@gmail.com>
2023-02-26 17:35:00 +0100

    Added logic to parse CSV data

--- src/csv/csv.service.ts
+++ src/csv/csv.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import fs from 'fs';
+import * as csv from 'csv';
 
 @Injectable()
-export class CsvService {}
+export class CsvService {
+    parseCSV(data, delimiter: string = ';'): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => {
+            csv.parse(data, { delimiter: delimiter }, (err, records, info) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                if (records) {
+                    // An assumption is made here that the document has only one column with the SKUs.
+                    // If there are more columns, we'd define a data model and map it to that.
+                    let mappedRecords: string[] = records.map((items) => items[0]);
+                    mappedRecords.shift();
+                    resolve(mappedRecords);
+                }
+
+                if (info) {
+                    console.log(`CSV parse info: ${info}`);
+                }
+            });
+        });
+    }
+
+    writeToCSV(data, delimiter: string = ';') { }
+}
