f144ce253f4860c33d22b3b12e824e01c8e806b9
stef0296 <steff.alvares@gmail.com>
2023-03-01 10:14:27 +0100

    Added imports and providers

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CsvService } from '../csv/csv.service';
+import { DbService } from '../db/db.service';
+import { ProductSchema } from '../schema/product.schema';
 import { ApiController } from './api.controller';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature(
+            [{ name: 'Product', schema: ProductSchema }]
+        ),
+    ],
     controllers: [ApiController],
-    providers: [CsvService]
+    providers: [CsvService, DbService]
 })
 export class ApiModule { }
