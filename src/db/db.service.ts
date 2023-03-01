eab27475923105034b4ee8d524a40c0a3da680f8
stef0296 <steff.alvares@gmail.com>
2023-03-01 10:14:10 +0100

    Added implementation to fetch data from db

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { InternalProduct } from '../interfaces/product.interface';
 
+// Reference: https://medium.com/globant/crud-application-using-nestjs-and-mongodb-99a0756adb76
 @Injectable()
-export class DbService {}
+export class DbService {
+    constructor(
+        @InjectModel('Product') private productRepository: Model<InternalProduct>
+    ) { }
+
+    async getProducts(skus: string[]): Promise<InternalProduct[]> {
+        const productData: InternalProduct[] = await this.productRepository.find({
+            sku: {
+                $in: skus
+            }
+        });
+        if (!productData || productData.length == 0) {
+            throw new NotFoundException('Products data not found!');
+        }
+        return productData;
+    }
+}
