b3ebf7975953ca3da1db642b8698ed1b789d5ed7
stef0296 <steff.alvares@gmail.com>
2023-03-04 14:51:25 +0100

    Updated translate service initializers

--- src/translate/translate.service.ts
+++ src/translate/translate.service.ts
@@ -7,16 +7,15 @@ export class TranslateService {
 
     constructor(
         private readonly configService: ConfigService,
-    ) {
-        translate.engine = "deepl";
-        translate.key = this.configService.get('DEEPL_KEY')
-    }
+    ) { }
 
     async translateContent(
         value: string,
         from: string = 'en',
         to: string = 'es'
     ): Promise<string> {
+        translate.engine = "deepl";
+        translate.key = this.configService.get('DEEPL_KEY')
         if (!value) return '';
         return await translate(value, { from: from, to: to });
     }
