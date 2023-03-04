8654d7d3dbd8dc7499e2e22ba946e42f411d92b6
stef0296 <steff.alvares@gmail.com>
2023-03-04 14:52:33 +0100

    Add scheduler implementation

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
+// Reference: https://docs.nestjs.com/techniques/task-scheduling
 @Injectable()
-export class SchedulerService {}
+export class SchedulerService {
+    private taskName: string = 'updateMarketPlaceCron';
+    constructor(
+        private readonly configService: ConfigService,
+        private schedulerRegistry: SchedulerRegistry
+    ) { }
+
+    runScheduledTask(runTask, skuData: string[]) {
+        let job: CronJob = this.schedulerRegistry.getCronJob(this.taskName);
+        let isFirstUpload = true;
+        if (job) {
+            this.schedulerRegistry.deleteCronJob(this.taskName);
+            isFirstUpload = false;
+        }
+
+        job = new CronJob(CronExpression.EVERY_10_MINUTES, () => {
+            runTask(skuData, isFirstUpload);
+        });
+
+        this.schedulerRegistry.addCronJob(this.taskName, job);
+        job.start();
+    }
+
+    cancelScheduledTask() {
+        const job = this.schedulerRegistry.getCronJob(this.taskName);
+        if (!job) {
+            job.stop();
+        }
+    }
+}
